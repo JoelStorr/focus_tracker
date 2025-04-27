e3bff4caa2b777076b952bab7353d3a8a3b5db3d
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2025-04-27 22:13:36 +0200

    Allowed option to stop the timer

--- lib/Screens/focus_timer.dart
+++ lib/Screens/focus_timer.dart
@@ -12,6 +12,8 @@ class FocusTimer extends StatefulWidget {
 class _FocusTimerState extends State<FocusTimer> {
   double timeframe = 0;
   int _roundTimeFrame = 0;
+  bool _hideSlider = false;
+  bool _stopTimer = false;
 
   void updateTimeFrame(double newTime) {
     setState(() {
@@ -22,12 +24,29 @@ class _FocusTimerState extends State<FocusTimer> {
 
   void updatedTime() {
     const duration = Duration(seconds: 1);
+
+    setState(() {
+      _hideSlider = true;
+    });
+
     Timer.periodic(duration, (Timer t) {
-      if (_roundTimeFrame == 1) t.cancel();
+      if (_roundTimeFrame == 1 || _stopTimer) {
+        t.cancel();
+        setState(() {
+          _hideSlider = false;
+          _stopTimer = false;
+        });
+      }
 
       setState(() {
         _roundTimeFrame -= 1;
       });
+    });
+  }
+
+  void stopTimer() {
+    setState(() {
+      _stopTimer = true;
     });
   }
 
@@ -41,12 +60,20 @@ class _FocusTimerState extends State<FocusTimer> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Text("$_roundTimeFrame"),
-            TimeSelector(
-              timeframe: timeframe,
-              updateTimeFrame: updateTimeFrame,
-            ),
+            if (!_hideSlider)
+              TimeSelector(
+                timeframe: timeframe,
+                updateTimeFrame: updateTimeFrame,
+              ),
 
-            TextButton(onPressed: updatedTime, child: Text("Start")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(onPressed: updatedTime, child: Text("Start")),
+                TextButton(onPressed: stopTimer, child: Text("Stop")),
+              ],
+            ),
           ],
         ),
       ),
